741f6f212ab26bbab6a429376bddabad0bb47ac9
divanshuraj <101104941+divanshuraj@users.noreply.github.com>
2022-03-07 18:23:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def xyz():
+     return render_template('app.html')
+@app.route('/info',methods=['POST'])
+def obj():
+    if(request.method=='POST'):
+        num1=request.form['a']
+        num2=request.form['b']
+        total=int(num1)+int(num2)
+        return render_template('app.html',total1=total)
+if __name__ == '__main__':
+    app.run()    
+
+
+# In[ ]:
+
+
+
+
